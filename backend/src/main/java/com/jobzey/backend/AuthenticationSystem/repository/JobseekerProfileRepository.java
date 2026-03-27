c1caaf039ade495ba2859efbaf95e30af62642a2
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-03-28 05:11:00 +0530

    feat: added jobseekerprofile repository

---
+++ backend/src/main/java/com/jobzey/backend/AuthenticationSystem/repository/JobseekerProfileRepository.java
@@ -0,0 +1,13 @@
+package com.jobzey.backend.AuthenticationSystem.repository;
+
+import com.jobzey.backend.AuthenticationSystem.model.JobseekerProfiles;
+import com.jobzey.backend.AuthenticationSystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JobseekerProfileRepository extends JpaRepository<Long, JobseekerProfiles> {
+    Optional<JobseekerProfiles> findByUser(User user);
+    Optional<JobseekerProfiles> findByUserId(String userId);
+    boolean existsByUserId(String userId);
+}
