cf416dff7431220d2953f3e179d34245e28f6d4a
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-03-27 07:20:00 +0530

    Created Repository for RecruiterProfile

---
+++ backend/src/main/java/com/jobzey/backend/AuthenticationSystem/repository/RecruiterProfileRepository.java
@@ -0,0 +1,13 @@
+package com.jobzey.backend.AuthenticationSystem.repository;
+
+import com.jobzey.backend.AuthenticationSystem.model.RecruiterProfile;
+import com.jobzey.backend.AuthenticationSystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Long> {
+    Optional<RecruiterProfile> findByUser (User user);
+    Optional<RecruiterProfile> findByUserId(String userId);
+    boolean existByUser(User user);
+}
