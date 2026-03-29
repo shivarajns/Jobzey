e6884f23c893557600c783ebfa2ca2d9e799f0bb
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-03-29 09:11:00 +0530

    feat: added jobseekereducation repository

---
+++ backend/src/main/java/com/jobzey/backend/AuthenticationSystem/repository/JobseekerEducationRepository.java
@@ -0,0 +1,15 @@
+package com.jobzey.backend.AuthenticationSystem.repository;
+
+import com.jobzey.backend.AuthenticationSystem.model.JobSeekerEducation;
+import com.jobzey.backend.AuthenticationSystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JobseekerEducationRepository extends JpaRepository<Long, JobSeekerEducation> {
+
+    Optional<JobSeekerEducation> findByUser(User user);
+    Optional<JobSeekerEducation> findByUserId(String userId);
+
+    void deleteByUser(User user);
+}
