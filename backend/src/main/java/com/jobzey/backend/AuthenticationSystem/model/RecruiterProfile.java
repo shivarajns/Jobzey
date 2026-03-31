460edfde8d3ab0f279bd232a0939ad16a9573ce4
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-04-01 01:39:48 +0530

    fix: fix recruiter profile by adding builder annotation.

--- backend/src/main/java/com/jobzey/backend/AuthenticationSystem/model/RecruiterProfile.java
+++ backend/src/main/java/com/jobzey/backend/AuthenticationSystem/model/RecruiterProfile.java
@@ -3,12 +3,14 @@ package com.jobzey.backend.AuthenticationSystem.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
