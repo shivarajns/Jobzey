aeba34a1631997d098594ef0e4527cb5f968cccc
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-05-31 09:14:43 +0530

    Update: Added all fields about the job in the model/entity layer class.

--- backend/src/main/java/com/jobzey/backend/JobListingSystem/DTO/GetJobByIdResponseDTO.java
+++ backend/src/main/java/com/jobzey/backend/JobListingSystem/DTO/GetJobByIdResponseDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +14,15 @@ import lombok.NoArgsConstructor;
 public class GetJobByIdResponseDTO {
     private String title;
     private String Description;
+    private int minSalary;
+    private int maxSalary;
+    private String currency;
+    private String location;
+    private String jobType;
+    private int experienceMin;
+    private int experienceMax;
+    private String status;
+    private LocalDateTime expiresAt;
+    private LocalDateTime createdAt;
+    private int categoryId;
 }
